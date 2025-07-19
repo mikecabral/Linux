0483bc028ccd670da53945bb42d5a65b3964a37d
Michael Cabral <mike.cabral@disroot.org>
2025-07-19 15:14:10 -0400

    remove .md from files

--- Mounting/mount-cifs-nfs.bash
+++ Mounting/mount-cifs-nfs.bash
@@ -1,9 +1,9 @@
 ```bash
-sudo mount -t cifs -o username=i5 //192.168.50.213/paperless /media/paperless
+sudo mount -t cifs -o username=i5 //192.168.x.x/paperless /media/paperless
 ```
 
 ```bash
-sudo mount -t cifs -o credentials=~/.i5cred //192.168.50.213/paperless /media/paperless
+sudo mount -t cifs -o credentials=~/.i5cred //192.168.x.x/paperless /media/paperless
 ```
 
 ```bash
@@ -12,7 +12,7 @@ FSTAB
 ```
 
 ```bash
-sudo mount -t cifs -o credentials=~/.i5cred,dir_mode=0755,file_mode=0755 //192.168.50.213/paperless_consume /media/paperless/consume
+sudo mount -t cifs -o credentials=~/.i5cred,dir_mode=0755,file_mode=0755 //192.168.x.x/paperless_consume /media/paperless/consume
 ```
 
 ##### **Make a file called either .smb or smb.cred with the following information:**
@@ -24,20 +24,20 @@ domain=WORKGROUP
 ```bash
 #i5 intel / paperless share
 # <file system>             <dir>          <type> <options>                                                   <dump>  <pass>
-//192.168.50.213/paperless_consume  /media/paperless/consume  cifs  credentials=/home/rhel/.i5cred,uid=1000,gid=1001,file_mode=0755,dir_mode=0755 0       0
+//192.168.x.x/paperless_consume  /media/paperless/consume  cifs  credentials=/home/rhel/.i5cred,uid=1000,gid=1001,file_mode=0755,dir_mode=0755 0       0
 ```
 
 ##### Mount NFS and FSTAB
 ```bash
-sudo mount -t nfs 192.168.50.154:/mnt/A/MP3 /media/music
+sudo mount -t nfs 192.168.x.x:/mnt/A/MP3 /media/music
 ```
 OR
 ```bash
-sudo mount -t nfs -o nfsvers=3 172.16.16.175:/mnt/A/MP3 /media/music
+sudo mount -t nfs -o nfsvers=3 172.16.16.x:/mnt/A/MP3 /media/music
 ```
 ##### Fstab file
 ```bash
-192.168.50.154:/mnt/A/MP3  /media/music nfs    defaults  0       0
+192.168.x.x:/mnt/A/MP3  /media/music nfs    defaults  0       0
 ```
 ##### Test to see if mounting works properly
 ```bash
@@ -55,17 +55,17 @@ df -h
 
 ##### Mount NFS Share WD MyCloud
 ```bash
-mount -t nfs 192.168.50.35:/nfs/Public /media/mycloud/
+mount -t nfs 192.168.x.x:/nfs/Public /media/mycloud/
 ```
 
 ##### Mount a cifs / samba share manually (ASKS FOR PASSWORD)
 ```bash
-mount -t cifs -o username=backupcreator //192.168.50.154/e /media/backup_server
+mount -t cifs -o username=backupcreator //192.168.x.x/e /media/backup_server
 ```
 
 ##### Mount a cifs / samba share manually (UNSECURE WAY)
 ```bash
-mount -t cifs -o username=backupcreator,password=pioneer //192.168.50.154/e /media/backup_server
+mount -t cifs -o username=backupcreator,password=pioneer //192.168.x.x/e /media/backup_server
 ```
 
 ##### Mount a cifs /samba share automatically and persistent with fstab
@@ -79,17 +79,17 @@ domain=WORKGROUP
 
 Open **/etc/fstab** and place at the bottom
 ```bash
-//192.168.50.154/e  /media/backup_server  cifs  credentials=/home/rockybackup/.smb  0 0
+//192.168.x.x/e  /media/backup_server  cifs  credentials=/home/rockybackup/.smb  0 0
 ```
 
 ##### USE EXPLICIT MOUNTING COMMAND WITH BASH SCRIPT
 ```bash
-mount -t cifs -o user=backupcreator,uid=1000,gid=1000 //192.168.50.154/e /media/backup_server 
+mount -t cifs -o user=backupcreator,uid=1000,gid=1000 //192.168.x.x/e /media/backup_server 
 ```
 
 ##### Mount FTP as a LOCAL DIR
 ```bash
-curlftpfs -o allow_other backupcreator:pioneer@192.168.50.154 /home/rockybackup/ftp 
+curlftpfs -o allow_other backupcreator:pioneer@192.168.x.x /home/rockybackup/ftp 
 ```
 OR
 ```bash
